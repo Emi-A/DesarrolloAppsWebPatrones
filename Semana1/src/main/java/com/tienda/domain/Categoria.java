45c6e3ab1cb9dade9b4f7eb630ce8239d787ab75
Emi-A <emilyam23@gmail.com>
2026-02-03 21:31:57 -0600

    Semana 4

--- Semana1/src/main/java/com/tienda/domain/Categoria.java
+++ Semana1/src/main/java/com/tienda/domain/Categoria.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.tienda.domain;
 
 import jakarta.persistence.Column;
@@ -8,35 +12,65 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-
+/**
+ *
+ * @author Jose Ortega
+ */
 @Entity
-@Getter
-@Setter
 @Table(name = "categoria")
 public class Categoria implements Serializable {
-
-    // Se recomienda añadir un serialVersionUID
     private static final long serialVersionUID = 1L;
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
-    public Integer idCategoria;
-
+    private Integer idCategoria;
+    
     @Column(unique = true, nullable = false, length = 50)
     @NotNull
     @Size(max = 50)
-    public String descripcion;
-
+    private String descripcion;
+    
     @Column(length = 1024)
     @Size(max = 1024)
-    public String rutaImagen;
-
+    private String rutaImagen;
+    
     @Column(name = "activo")
-    public Boolean activo;
+    private Boolean activo;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
+ 
